bfbd109a1bfa4152bab7055c9218fe02cc2df5e8
Administrator <nagiyevintiqam@gmail.com>
2020-03-13 16:40:05 +0400

    ---

---
+++ src/main/java/model/Passenger.java
@@ -0,0 +1,26 @@
+package model;
+
+import java.io.Serializable;
+
+public class Passenger implements Serializable {
+    private String name;
+    private  String surname;
+
+    public Passenger(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(" Name:%-10s Surname:%-10s\n", name , surname);
+    }
+}
